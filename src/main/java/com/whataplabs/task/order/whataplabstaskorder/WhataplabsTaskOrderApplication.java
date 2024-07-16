3a4b411396cdd21d0c2ce4a584baacd582b2d57f
hanbee <sonhanbi1002@gmail.com>
2024-07-17 00:27:19 +0900

    init

---
+++ src/main/java/com/whataplabs/task/order/whataplabstaskorder/WhataplabsTaskOrderApplication.java
@@ -0,0 +1,13 @@
+package com.whataplabs.task.order.whataplabstaskorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhataplabsTaskOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhataplabsTaskOrderApplication.class, args);
+    }
+
+}
