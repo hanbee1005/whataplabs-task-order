ab9eea47a5899754ee80fd3cd138c68a69ecc648
hanbee <sonhanbi1002@gmail.com>
2024-07-18 13:32:28 +0900

    feat: RestTemplate timeout 설정

--- src/main/java/com/whataplabs/task/order/whataplabstaskorder/infrastructure/config/AppConfig.java
+++ src/main/java/com/whataplabs/task/order/whataplabstaskorder/infrastructure/config/AppConfig.java
@@ -1,16 +1,9 @@
 package com.whataplabs.task.order.whataplabstaskorder.infrastructure.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.client.RestTemplate;
 
 @EnableAsync
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
